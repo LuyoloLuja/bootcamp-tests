5c9d4811138d63780b80ba6db1ba436f8101fd7c
LuyoloLuja <luyolom19@gmail.com>
2020-04-01 12:26:00 +0200

    the howManyYears function

--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,4 +1,8 @@
 describe('The yearsAgo function', function(){
+    it('Should count number of years from 1976 to 2000', function(){
+        var years = 2000 - 1976;
+        assert.equal(years, 24)
+    })
     it('Should count number of years from 1976 to 2020', function(){
         var years = yearsAgo(1976, 2020)
         assert.equal(years, 44);
